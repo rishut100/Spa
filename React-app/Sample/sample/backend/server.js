0db52c934378f1577d686103e780052242cacbeb
Rishu Tiwari <54594441+rishut100@users.noreply.github.com>
2020-08-11 22:20:32 +0530

    Update server.js

--- React-app/Sample/sample/backend/server.js
+++ React-app/Sample/sample/backend/server.js
@@ -12,11 +12,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB database connection established successfully");
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+// const connection = mongoose.connection;
+// connection.once("open", () => {
+//   console.log("MongoDB database connection established successfully");
+// });
+
+const url = "mongodb://localhost:27017/sample";
+mongoose.connect(url, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.once("open", (_) => {
+  console.log("Database connected:", url);
 });
 
 app.use("/exercises", exercisesRouter);
